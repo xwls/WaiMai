7e7be9dfbf334472301d26c7bb421ab50bd554c2
Kevin-OAEC <351916362@qq.com>
2016-10-08 19:46:47 +0800

    调整目录结构

--- app/src/main/java/com/oaec/waimai/activity/MainActivity.java
+++ app/src/main/java/com/oaec/waimai/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.oaec.waimai;
+package com.oaec.waimai.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.oaec.waimai.R;
 
 public class MainActivity extends AppCompatActivity {
 
